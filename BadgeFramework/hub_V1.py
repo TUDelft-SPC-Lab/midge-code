423a3af0e8781444d351cf62ab27cf82b2e2ec41
Era Dorta <e.dorta@tudelft.nl>
2025-05-14 15:39:13 +0200

    fix: reduce code duplication in hub_V1.py

--- BadgeFramework/hub_V1.py
+++ BadgeFramework/hub_V1.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import sys
-from hub_utilities_V1 import *
+from hub_utilities_V1 import (
+    timeout_input,
+    start_recording_all_devices,
+    stop_recording_all_devices,
+    synchronise_and_check_all_devices,
+    choose_function,
+    Connection,
+)
 
 def print_hub_commands():
     print(" start_all: starts recording on all midges with all sensors")
@@ -27,23 +34,15 @@ if __name__ == "__main__":
                 print("Devices are already recording.")
                 sys.stdout.flush()
             else:
-                print("Start recording of all devices.")
-                sys.stdout.flush()
                 start_recording_all_devices(df)
                 do_synchronization = True
-                print("Devices are recording.")
-                sys.stdout.flush()
         elif command == "stop_all":
             if do_synchronization is False:
                 print("Devices are not recording.")
                 sys.stdout.flush()
             else:
-                print("Stopping the recording of all devices.")
-                sys.stdout.flush()
                 do_synchronization = False
                 stop_recording_all_devices(df)
-                print("Devices are stopped.")
-                sys.stdout.flush()
         elif command == "help":
             print_hub_commands()
             sys.stdout.flush()
@@ -56,12 +55,8 @@ if __name__ == "__main__":
                                    extend_timeout_with_input=False, require_enter_to_confirm=True)
 
                 if command == "":
-                    print("Sync in single midge selection")
                     if do_synchronization is True:
-                        print('Synchronisation is starting. Please wait until it ends.')
                         synchronise_and_check_all_devices(df)
-                        print('Synchronisation is finished.')
-                        sys.stdout.flush()
                 elif command == "exit":
                     print("Exited single midge management")
                     sys.stdout.flush()
@@ -82,44 +77,35 @@ if __name__ == "__main__":
                         command_args = command.split(" ")
                         if command == "exit":
                             cur_connection.disconnect()
-                            print(" Midge disconnected")
+                            print("Midge disconnected")
                             sys.stdout.flush()
                             break
-                        if command != "":
+                        elif command == "":
+                            print("Sync in single midge connected")
+                            if do_synchronization is True:
+                                synchronise_and_check_all_devices(df, skip_id=midge_id, conn_skip_id=cur_connection)
+                        elif command != "":
                             try:
                                 out = choose_function(cur_connection, command_args[0])
-                                if out != None:
+                                if out is not None:
                                     print (out)
                                     sys.stdout.flush()
                             except Exception as error:
                                 print (str(error))
-                                print(" Command not found!")
+                                print("Command not found!")
                                 sys.stdout.flush()
                                 cur_connection.print_help()
                                 continue
-                        elif command == "":
-                            print("Sync in single midge connected")
-                            if do_synchronization is True:
-                                print('Synchronisation is starting. Please wait till it ends.')
-                                synchronise_and_check_all_devices(df, skip_id=midge_id, conn_skip_id=cur_connection)
-                                print('Synchronisation is finished.')
-                                sys.stdout.flush()
-        elif command == "":
-            if do_synchronization is True:
-                print('Synchronisation is starting. Please wait till it ends.')
-                synchronise_and_check_all_devices(df)
-                print('Synchronisation is finished.')
-                sys.stdout.flush()
         elif command == "exit":
             print("Exit hub script.")
             sys.stdout.flush()
             if do_synchronization:
-                print("Stopping the recording of all devices.")
-                sys.stdout.flush()
                 do_synchronization = False
                 stop_recording_all_devices(df)
-                print("Devices are stopped.")
             quit(0)
+        elif command == "":
+            if do_synchronization is True:
+                synchronise_and_check_all_devices(df)
         else:
             print('Unknown command.')
             sys.stdout.flush()
