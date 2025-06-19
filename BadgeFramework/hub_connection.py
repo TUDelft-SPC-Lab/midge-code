5b00837ded45bb421fe065de5799ce1f881d48f8
Era Dorta <e.dorta@tudelft.nl>
2025-06-19 11:39:57 +0200

    fix: if failed to connect, try again 3 times

--- BadgeFramework/hub_connection.py
+++ BadgeFramework/hub_connection.py
@@ -4,19 +4,24 @@ import sys
 constant_group_number = 1
 
 class Connection():
-    def __init__(self,pid,address):
-        try:
-            self.connection = BLEBadgeConnection.get_connection_to_badge(address)
-            self.connection.connect()
-            self.badge = OpenBadge(self.connection)
-            self.badge_id = int(pid)
-            self.mac_address = address
-            self.group_number = int(constant_group_number)
-        except Exception as err:
-            #if (err):
-            #    print (str(err))
-            raise Exception("Could not connect to participant" + str(pid))
+    def __init__(self,pid,address, num_tries=3):
+        for i in range(num_tries):
+            try:
+                self.connection = BLEBadgeConnection.get_connection_to_badge(address)
+                self.connection.connect()
+                self.badge = OpenBadge(self.connection)
+                self.badge_id = int(pid)
+                self.mac_address = address
+                self.group_number = int(constant_group_number)
+                break
+            except Exception as err:
+                if i >= num_tries - 1:
+                    raise Exception(
+                        "Could not connect to participant " + str(pid) + ", error: "
+                        + str(err)
+                    )
 
+        
     def set_id_at_start(self):
         try:
             self.badge.get_status(new_id=self.badge_id, new_group_number=self.group_number)
